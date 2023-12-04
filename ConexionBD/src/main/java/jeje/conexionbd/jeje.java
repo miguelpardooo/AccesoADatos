ba9e8e57b8e0cfbb37478d8f4e578699533b5b50
miguelpardooo <114778564+miguelpardooo@users.noreply.github.com>
2023-12-04 20:47:07 +0100

    Add files via upload

---
+++ ConexionBD/src/main/java/jeje/conexionbd/jeje.java
@@ -0,0 +1,20 @@
+package jeje.conexionbd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class jeje {
+
+    
+    
+    public static void main(String[] args) {
+        
+        
+    }
+    
+    
+}
